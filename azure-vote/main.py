b101a165058c704da2e0d29738cf88bf06b8d8af
Trung <trung4416180@gmail.com>
2023-11-07 18:31:25 +0700

    update main

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -79,20 +79,8 @@ if ("TITLE" in os.environ and os.environ['TITLE']):
 else:
     title = app.config['TITLE']
 
-# Redis configurations
-redis_server = os.environ['REDIS']
-
 # Redis Connection
-try:
-    if "REDIS_PWD" in os.environ:
-        r = redis.StrictRedis(host=redis_server,
-                        port=6379,
-                        password=os.environ['REDIS_PWD'])
-    else:
-        r = redis.Redis(redis_server)
-    r.ping()
-except redis.ConnectionError:
-    exit('Failed to connect to Redis, terminating.')
+r = redis.Redis()
 
 # Change title to host name to demo NLB
 if app.config['SHOWHOST'] == "true":
@@ -161,6 +149,6 @@ def index():
 
 if __name__ == "__main__":
     # TODO: Use the statement below when running locally
-    app.run()
+    # app.run()
     # TODO: Use the statement below before deployment to VMSS
-    # app.run(host='0.0.0.0', threaded=True, debug=True) # remotea
+    app.run(host='0.0.0.0', threaded=True, debug=True) # remotea
