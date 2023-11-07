5478a4daed7a72331277578f9667a3e326c44cce
Trung <trung4416180@gmail.com>
2023-11-07 13:29:48 +0700

    Initial commit for Deploy_to_VMSS branch

--- setup-script.sh
+++ setup-script.sh
@@ -3,7 +3,7 @@
 # Variables
 resourceGroup="acdnd-c4-project"
 location="westus"
-osType="UbuntuLTS"
+osType="Ubuntu2204"
 vmssName="udacity-vmss"
 adminName="udacityadmin"
 storageAccount="udacitydiag$RANDOM"
@@ -24,8 +24,8 @@ storageType="Standard_LRS"
 echo "STEP 0 - Creating resource group $resourceGroup..."
 
 az group create \
---name $resourceGroup \
---location $location \
+--name cdnd-c4-project \
+--location westus \
 --verbose
 
 echo "Resource group created: $resourceGroup"
@@ -34,19 +34,19 @@ echo "Resource group created: $resourceGroup"
 echo "STEP 1 - Creating storage account $storageAccount"
 
 az storage account create \
---name $storageAccount \
---resource-group $resourceGroup \
---location $location \
+--name udacitydiag008 \
+--resource-group cdnd-c4-project \
+--location westus \
 --sku Standard_LRS
 
 echo "Storage account created: $storageAccount"
 
 # Create Network Security Group
-echo "STEP 2 - Creating network security group $nsgName"
+echo "STEP 2 - Creating network security group udacity-vmss-nsg"
 
 az network nsg create \
---resource-group $resourceGroup \
---name $nsgName \
+--resource-group cdnd-c4-project \
+--name udacity-vmss-nsg \
 --verbose
 
 echo "Network security group created: $nsgName"
@@ -55,19 +55,19 @@ echo "Network security group created: $nsgName"
 echo "STEP 3 - Creating VM scale set $vmssName"
 
 az vmss create \
-  --resource-group $resourceGroup \
-  --name $vmssName \
-  --image $osType \
-  --vm-sku $vmSize \
-  --nsg $nsgName \
-  --subnet $subnetName \
-  --vnet-name $vnetName \
-  --backend-pool-name $bePoolName \
-  --storage-sku $storageType \
-  --load-balancer $lbName \
+  --resource-group cdnd-c4-project \
+  --name udacity-vmss \
+  --image Ubuntu2204 \
+  --vm-sku Standard_B1s \
+  --nsg udacity-vmss-nsg \
+  --subnet udacity-vmss-vnet-subnet \
+  --vnet-name udacity-vmss-vnet \
+  --backend-pool-name udacity-vmss-bepool \
+  --storage-sku Standard_LRS \
+  --load-balancer udacity-vmss-lb \
   --custom-data cloud-init.txt \
   --upgrade-policy-mode automatic \
-  --admin-username $adminName \
+  --admin-username udacityadmin \
   --generate-ssh-keys \
   --verbose 
 
@@ -77,10 +77,10 @@ echo "VM scale set created: $vmssName"
 echo "STEP 4 - Associating NSG: $nsgName with subnet: $subnetName"
 
 az network vnet subnet update \
---resource-group $resourceGroup \
---name $subnetName \
---vnet-name $vnetName \
---network-security-group $nsgName \
+--resource-group cdnd-c4-project \
+--name udacity-vmss-vnet-subnet \
+--vnet-name udacity-vmss-vnet \
+--network-security-group udacity-vmss-nsg \
 --verbose
 
 echo "NSG: $nsgName associated with subnet: $subnetName"
@@ -89,9 +89,9 @@ echo "NSG: $nsgName associated with subnet: $subnetName"
 echo "STEP 5 - Creating health probe $probeName"
 
 az network lb probe create \
-  --resource-group $resourceGroup \
-  --lb-name $lbName \
-  --name $probeName \
+  --resource-group cdnd-c4-project \
+  --lb-name udacity-vmss-lb \
+  --name tcpProbe \
   --protocol tcp \
   --port 80 \
   --interval 5 \
@@ -104,11 +104,11 @@ echo "Health probe created: $probeName"
 echo "STEP 6 - Creating network load balancer rule $lbRule"
 
 az network lb rule create \
-  --resource-group $resourceGroup \
-  --name $lbRule \
-  --lb-name $lbName \
-  --probe-name $probeName \
-  --backend-pool-name $bePoolName \
+  --resource-group cdnd-c4-project\
+  --name udacity-vmss-lb-network-rule \
+  --lb-name udacity-vmss-lb \
+  --probe-name tcpProbe \
+  --backend-pool-name udacity-vmss-bepool \
   --backend-port 80 \
   --frontend-ip-name loadBalancerFrontEnd \
   --frontend-port 80 \
@@ -121,8 +121,8 @@ echo "Network load balancer rule created: $lbRule"
 echo "STEP 7 - Adding port 80 to NSG $nsgName"
 
 az network nsg rule create \
---resource-group $resourceGroup \
---nsg-name $nsgName \
+--resource-group cdnd-c4-project \
+--nsg-name udacity-vmss-nsg \
 --name Port_80 \
 --destination-port-ranges 80 \
 --direction Inbound \
@@ -135,8 +135,8 @@ echo "Port 80 added to NSG: $nsgName"
 echo "STEP 8 - Adding port 22 to NSG $nsgName"
 
 az network nsg rule create \
---resource-group $resourceGroup \
---nsg-name $nsgName \
+--resource-group cdnd-c4-project \
+--nsg-name udacity-vmss-nsg \
 --name Port_22 \
 --destination-port-ranges 22 \
 --direction Inbound \
